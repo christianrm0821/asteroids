06103cd8306649e07bcbec6205b7e3ac51ae1d02
christianrm0821 <christianrm0821@gmail.com>
2024-10-14 13:53:59 -0700

    circleshape.py,constants.py, player.py added and showing player on screen as a triangle

--- main.py
+++ main.py
@@ -3,21 +3,22 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    my_player = Player(SCREEN_WIDTH/2 , SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        my_player.draw(screen)
         pygame.display.flip()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
